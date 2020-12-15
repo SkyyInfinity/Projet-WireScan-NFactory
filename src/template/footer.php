c0d57b49543a6e652cc2afe822090c836ed9dc29
legilmalas <florian.galvani@gmail.com>
2020-12-15 10:38:41 +0100

    Implementation de la modal de connexion/deconnexion

--- src/template/footer.php
+++ src/template/footer.php
@@ -10,6 +10,9 @@
         </footer>
 
         <!-- JAVASCRIPT -->
-        <script src="./assets/js/app.js" type="text/javascript" charset="utf-8"></script>
+        <script src="assets/js/jquery.js" type ="text/javascript" charset="utf-8"></script>
+        <script src="assets/js/app.js" type="text/javascript" charset="utf-8"></script>
+        <script src="src/plugins/jquery_modal/jquery.modal.js" type ="text/javascript" charset="utf-8"></script>
+        
     </body>
 </html>
--- src/template/header.php
+++ src/template/header.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="max 156 caractères">
         <meta name="keywords" content="html,css,formation">
-        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="src/plugins/jquery_modal/jquery.modal.css">
         <title>title</title>
     </head>
     <body>
