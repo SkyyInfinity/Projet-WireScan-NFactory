f8fcb6ce3eaabbf94500ec67db5103f9e9f94128
FlorianGalvani <florian.galvani@gmail.com>
2021-01-12 12:00:05 +0100

    modif

--- dashboard/ajax/addtrame.php
+++ dashboard/ajax/addtrame.php
@@ -6,35 +6,34 @@ $errors = array();
 $success = false;
 
 $user_id = $_SESSION['user']['id'];
-$json = $_POST['trame']; // récuperation du contenu du fichier trame
+$trames = $_POST['trame']; // récuperation du contenu du fichier trame
 $from = trim(strip_tags($_POST['from']));
 //$user_id = "45";
 //$from = 'jsonurl';
 //$json = file_get_contents('tram.txt');
 // JSON vers string
-// debug($json); 
-
 if ($from == "jsonurl") {
-    if (!empty($json)) {
+    if (!empty($trames)) {
+
     } else {
         $errors['json'] = 'Veuillez renseigner le champ';
     }
 } elseif ($from == "jsoninput") {
-    if (!empty($json)) {
-        $json = json_decode($json, true);
+    if (!empty($trames)) {
+        $trames = json_decode($trames, true);
     } else {
         $errors['json'] = 'Veuillez ajouter un fichier .json';
     }
 } elseif ($from == "jsonTA") {
     if (!empty($json)) {
-        $json = json_decode($json, true);
+        $trames = json_decode($trames, true);
     } else {
         $errors['json'] = 'Veuillez renseigné le champ';
     }
 }
 if(count($errors) == 0 ) {
     $success = true;
-    breakJSONToSQL($json,$user_id);
+    breakJSONToSQL($trames,$user_id);
 }
 $data = array(
     'errors' => $errors,
