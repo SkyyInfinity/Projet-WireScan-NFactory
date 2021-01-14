7b00993a0f0480a5337ae4e79669be0c9639a738
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2021-01-14 17:01:42 +0100

    fix responsive

--- dashboard/index.php
+++ dashboard/index.php
@@ -88,7 +88,7 @@ include('src/template/db_header.php');
   <div class="notrames" id="notrames">
     <p> Vous n'avez pas de trames pour le moment </p>
     <p> Veuillez en ajouter une en utilisant le bouton ci-dessous </p>
-    <a href="#" class="btn_addtrame" id="btn_addtrame_no">Ajouter une trame<a>
+    <a href="#" class="btn_addtrame" id="btn_addtrame_no">Ajouter une trame</a>
   </div>
 </div>
 <?php include('src/template/db_footer.php'); ?>
